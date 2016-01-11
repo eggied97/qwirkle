c6f5bfc0e03739c0e2ea77de80cd5a8c56a18af4
Sander011 <sander_meinderts@hotmail.com>
2016-01-11 11:07:06 +0100

    bagToHand method

--- qwirkle/src/nl/utwente/ewi/qwirkle/model/player/Player.java
+++ qwirkle/src/nl/utwente/ewi/qwirkle/model/player/Player.java
@@ -1,15 +1,19 @@
 package nl.utwente.ewi.qwirkle.model.player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.awt.Point;
 import nl.utwente.ewi.qwirkle.model.*;
 
 public abstract class Player {
 	
 	private String name;
+	private List<Tile> hand;
 	
 	public Player(String name) {
 		this.name = name;
+		this.hand = new ArrayList<>();
 	}
 	
 	public String getName() {
@@ -21,6 +25,11 @@ public abstract class Player {
 	public void makeMove(Board board) {
 		HashMap<Point, Tile> move = determineMove(board);
 		// TODO WAIT FOR CONFIRMATION FROM SERVER
+	}
+	
+	public void bagToHand(List<Tile> tiles) {
+		hand.addAll(tiles);
+		// TODO FROM SERVER CALL THIS WITH A LIST OF TILES
 	}
 	
 	
