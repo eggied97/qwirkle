230c3f0abe8f106b1f3e1e8230fc5a207553fb65
Egbert Dijkstra <info@egbert-dijkstra.nl>
2016-01-17 11:03:15 +0100

    we have a working dumb AI now :D

--- qwirkle/src/nl/utwente/ewi/qwirkle/model/player/strategy/DumbStrategy.java
+++ qwirkle/src/nl/utwente/ewi/qwirkle/model/player/strategy/DumbStrategy.java
@@ -9,7 +9,7 @@ import nl.utwente.ewi.qwirkle.model.Point;
 import nl.utwente.ewi.qwirkle.model.Tile;
 import nl.utwente.ewi.qwirkle.server.ValidMove;
 
-public class DumbStrategy implements Strategy{
+public class DumbStrategy implements Strategy {
 
 	@Override
 	public String getName() {
@@ -19,31 +19,29 @@ public class DumbStrategy implements Strategy{
 	@Override
 	public List<Move> determineMove(Board b, List<Tile> hand) {
 		List<Move> result = new ArrayList<>();
-		
-		
-		boolean moveIsValid = false;		
+
+		boolean moveIsValid = false;
 		List<Point> emptySpots = b.getEmptySpots();
 		ValidMove vm = new ValidMove();
-		
-		for(int i = 0; i < hand.size() || moveIsValid; i++){
-			
-			Tile t =hand.get(i);
-			
-//TODO remove this whilelus
-			while(!moveIsValid){				
-				//check every empty spot
-				for(Point p : emptySpots){
+
+		for (int i = 0; i < hand.size() && !moveIsValid; i++) {
+
+			Tile t = hand.get(i);
+
+			// check every empty spot
+			for (Point p : emptySpots) {
+				if (!moveIsValid) {
 					Move m = new Move(p, t);
-					
+
 					moveIsValid = vm.isValidMove(m, b);
-					
-					if(moveIsValid){
+
+					if (moveIsValid) {
 						result.add(m);
 					}
 				}
 			}
 		}
-		
+
 		return result;
 	}
 
