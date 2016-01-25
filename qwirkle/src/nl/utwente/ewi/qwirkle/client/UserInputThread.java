c17e1b7789a54e803e8706192c89f4a313e2ff05
Egbert Dijkstra <info@egbert-dijkstra.nl>
2016-01-25 18:35:34 +0100

    javadoced UIT, client package is done now

--- qwirkle/src/nl/utwente/ewi/qwirkle/client/UserInputThread.java
+++ qwirkle/src/nl/utwente/ewi/qwirkle/client/UserInputThread.java
@@ -5,20 +5,46 @@ import java.util.Scanner;
 import nl.utwente.ewi.qwirkle.callback.UserInputCallback;
 import nl.utwente.ewi.qwirkle.model.enums.InputState;
 
+/**
+ * This Thread handles the user input when using the TUI view.
+ * 
+ * This enables us to chat while it is not our turn.
+ */
+
 public class UserInputThread extends Thread {
 
 	private boolean running = true;
 	private InputState state = InputState.FORSERVERINFORMATION;
 	private UserInputCallback callback;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param callback
+	 *            the callback it uses to send the text to.
+	 */
 	public UserInputThread(UserInputCallback callback) {
 		this.callback = callback;
 	}
 
+	/**
+	 * sets the running state.
+	 * 
+	 * @param b
+	 *            the running state
+	 */
 	public void setRunning(Boolean b) {
 		this.running = b;
 	}
-	
+
+	/**
+	 * Sets the {@link nl.utwente.ewi.qwirkle.model.enums.InputState}, to
+	 * determine the context.
+	 * 
+	 * @param s
+	 *            the {@link nl.utwente.ewi.qwirkle.model.enums.InputState}
+	 *            instance
+	 */
 	public void setInputState(InputState s) {
 		this.state = s;
 	}
